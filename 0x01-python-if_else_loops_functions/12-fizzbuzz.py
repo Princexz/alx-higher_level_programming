4cfbdd15f97c884863b29208be8977f859c27720
Prince Solomon <77865154+Princexz@users.noreply.github.com>
2023-03-07 23:30:49 +0100

    0x01-python-if_else_loops_functions-FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-# Author - Bamidele Adefolaju
+# Author - Prince Solomon
 """Print the numbers from 1 to 100 separated by a space.
   For multiples of three, print Fizz instead of the number
   For multiples of five, print Buzz instead of the number.
   For multiples of three and five, print FizzBuzz instead of the number.
   """
-
 
 def fizzbuzz():
     for number in range(1, 101):
